a6284f94bb32d73983f53326d57af65109e213e3
Dom <dominicsmith1021@gmail.com>
2018-12-11 10:47:45 -0500

    Setting up other trolls

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -39,15 +39,23 @@ function addAnim(creature, animName, frames, size, bgX, bgY, wait, plays) {
     creatures[creature] = newCreature;
 };
 
-addAnim("troll_1","Idle", 10, "65%", "65%", "60%", 3, "loop");
-addAnim("troll_1","Walk", 10, "72%", "73%", "60%", 4, "loop");
-addAnim("troll_1","Dead", 10, "73%", "88%", "79%", 4, 1);
-addAnim("troll_1","Hurt", 10, "73%", "43%", "50%", 4, 1);
-addAnim("troll_1","Attack", 10, "100%", "", "", 4, 1);
+var testCreature = "troll_3";
 
-creatures["troll_1"].currentAnim = "walk";
+// addAnim("troll_1","Idle", 10, "65%", "65%", "60%", 3, "loop");
+// addAnim("troll_1","Walk", 10, "72%", "73%", "60%", 4, "loop");
+// addAnim("troll_1","Dead", 10, "73%", "88%", "79%", 4, 1);
+// addAnim("troll_1","Hurt", 10, "73%", "43%", "50%", 4, 1);
+// addAnim("troll_1","Attack", 10, "100%", "", "", 4, 1);
 
 troll.css("left","-45%");
+
+addAnim(testCreature,"Idle", 10, "65%", "65%", "60%", 3, "loop");
+addAnim(testCreature,"Walk", 9, "72%", "73%", "60%", 4, "loop");
+addAnim(testCreature,"Dead", 10, "73%", "88%", "79%", 4, 1);
+addAnim(testCreature,"Hurt", 10, "73%", "43%", "50%", 4, 1);
+addAnim(testCreature,"Attack", 10, "100%", "", "", 4, 1);
+
+creatures[testCreature].currentAnim = "walk";
 
 var animInt = setInterval(function(){ 
     var arr = Object.keys(creatures);
@@ -64,7 +72,7 @@ var animInt = setInterval(function(){
                 anim.frame = 0;
                 if (creature.currentAnim == "dead") {
                     anim.frame = anim.keyFrames.length-1;
-                    troll.slideUp();
+                    // troll.slideUp();
                 }
                 else if (anim.plays != "loop") {
                     creature.currentAnim = "idle";
@@ -77,27 +85,27 @@ var animInt = setInterval(function(){
 },17);
 
 troll.animate({"left":"20%"},5000,"linear", function() {
-    creatures["troll_1"].currentAnim = "idle";
+    creatures[testCreature].currentAnim = "idle";
 });
 
 
 $(document).on("keyup", function(event) {
     switch (event.key) {
         case " ":
-            creatures["troll_1"].currentAnim = "attack";
+            creatures[testCreature].currentAnim = "attack";
         break;
         case "w":
-            creatures["troll_1"].currentAnim = "walk";
+            creatures[testCreature].currentAnim = "walk";
         break;
         case "h":
-            creatures["troll_1"].currentAnim = "hurt";
+            creatures[testCreature].currentAnim = "hurt";
         break;
         case "d":
-            creatures["troll_1"].currentAnim = "dead";
+            creatures[testCreature].currentAnim = "dead";
         break;
         case "b":
-            creatures["troll_1"].currentAnim = "break";
+            creatures[testCreature].currentAnim = "break";
         break;
     };
-    creatures["troll_1"].currFrame = 0;
+    creatures[testCreature].currFrame = 0;
 });
