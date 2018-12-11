ae31ea04655c2f01c5bd9d7cb44665366bcd00b0
Dom <dominicsmith1021@gmail.com>
2018-12-10 20:32:06 -0500

    Testing sprite animations

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,15 +2,22 @@
 
 var troll = document.getElementById('troll');
 var anims = [];
-const images = "assets/images/troll_1/";
+const images = "assets/images/";
 
-function addAnim() {
-    for (var i = 0; i < arguments.length; i++) {
-        anims.push(images + arguments[i]);
+function addAnim(creature, animName,frames) {
+    for (var i = 0; i < frames; i++) {
+        let pos = i;
+        if (i < 10) {
+            pos = "00" + i;
+        }
+        else if (i < 100) {
+            pos = "0" + i;
+        }
+        anims.push(images + creature + "/" + animName + "_" + pos + ".png");
     };
 };
 
-addAnim("Idle_000","Idle_001","Idle_002","Idle_003","Idle_004","Idle_005","Idle_006","Idle_007","Idle_008","Idle_009");
+addAnim("troll_1","Idle", 10);
 
 var num = 0;
 var animInt = setInterval(function(){ 
@@ -18,5 +25,5 @@ var animInt = setInterval(function(){
     if (num >= anims.length) {
         num = 0;
     };
-    troll.src = anims[num] + ".png";
+    troll.src = anims[num];
 },60);
