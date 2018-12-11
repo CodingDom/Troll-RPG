18d69de793354bec492525db1d5ddc8818e3d467
Dom <dominicsmith1021@gmail.com>
2018-12-10 21:41:43 -0500

    Testing attack animation

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,24 @@
 //For future reference: 76 x 88
 
 var troll = document.getElementById('troll');
-var anims = [];
+var creatures = {};
 const images = "assets/images/";
 
-function addAnim(creature, animName,frames) {
+function addAnim(creature, animName, frames, size, wait, plays) {
+    //Checking for existing creature name
+    var newCreature = creatures[creature];
+    if (!newCreature) {
+        newCreature = {};
+    };
+    const originName = animName; //For grabbing the image name
+
+    animName = animName.toLowerCase();
+    newCreature[animName] = {};
+    newCreature[animName].keyFrames = [];
+    newCreature[animName].size = size;
+    newCreature[animName].wait = wait;
+    newCreature[animName].frame = 0;
+    newCreature[animName].plays = plays;
     for (var i = 0; i < frames; i++) {
         let pos = i;
         if (i < 10) {
@@ -13,17 +27,40 @@ function addAnim(creature, animName,frames) {
         else if (i < 100) {
             pos = "0" + i;
         }
-        anims.push(images + creature + "/" + animName + "_" + pos + ".png");
+        newCreature[animName].keyFrames.push(images + creature + "/" + originName + "_" + pos + ".png");
     };
+    creatures[creature] = newCreature;
 };
 
-addAnim("troll_1","Idle", 10);
+addAnim("troll_1","Idle", 10, "45%", 3, "loop");
+addAnim("troll_1","Attack", 10, "85%", 3, 1);
 
-var num = 0;
+creatures["troll_1"].currentAnim = "idle";
+
+var currFrame = 0;
 var animInt = setInterval(function(){ 
-    num++;
-    if (num >= anims.length) {
-        num = 0;
+    var arr = Object.keys(creatures);
+    for (var i = 0; i < arr.length; i++) {
+        var creature = creatures[arr[i]];
+        var anim = creature[creature.currentAnim];
+        if (currFrame % anim.wait == 0) {
+            troll.src = anim.keyFrames[anim.frame];
+            troll.style.height = anim.size;
+            anim.frame++;
+            if (anim.frame >= anim.keyFrames.length ) {
+                anim.frame = 0;
+                if (anim.plays != "loop") {
+                    creature.currentAnim = "idle";
+                }
+            };
+        }
     };
-    troll.src = anims[num];
-},60);
+    currFrame++;
+},20);
+
+
+document.onkeyup = function(event) {
+    if (event.key == " ") {
+        creatures["troll_1"].currentAnim = "attack";
+    }
+}
